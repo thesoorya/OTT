38ca53a98c146914a70dab055ee69ecb51b4b1e1
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-23 13:15:10 +0530

    jwt token generator function created

---
+++ server/utils/generateToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+exports.generateTokenAndCookie = async (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' })
+
+    res.cookie('jwt-ott', token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV !== 'development'
+    })
+
+    return token
+}
