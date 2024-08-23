209d9eee71e1f820b80e36d2beca05a390933514
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-23 15:57:36 +0530

    movie route api

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db')
 const authRoute = require('./routes/authRoute')
+const movieRoute = require('./routes/movieRoute')
 const PORT = process.env.PORT || 5000
 
 connectDB()
@@ -13,7 +14,7 @@ app.use(cors())
 app.use(cookieParser())
 
 app.use('/api/auth', authRoute)
-
+app.use('/api/movie', movieRoute)
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
