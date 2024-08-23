059ced56930e354a7306951ab90283b5d4b8950d
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-23 13:13:56 +0530

    server created and db connected

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db')
+const authRoute = require('./routes/authRoute')
+const PORT = process.env.PORT || 5000
+
+connectDB()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use('/api/auth', authRoute)
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+})
