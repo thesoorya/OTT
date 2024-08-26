2b3ec4e075d16bec545116bd292a955fa95c25b9
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-26 17:20:47 +0530

    deleted files and minor modifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Signup from './pages/Auth/Signup'
+import Login from './pages/Auth/Login'
+import Home from './pages/Home/Home'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Toaster />
+    </div>
   )
 }
 
