dce95a04008a1d796784fbe70211101b6ec53434
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-27 16:46:16 +0530

    backbar completed

--- client/src/components/BackBar/BackBar.jsx
+++ client/src/components/BackBar/BackBar.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { IoIosArrowBack } from "react-icons/io";
-import './BackBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import './BackBar.css'
 
 const BackBar = () => {
-  const navigate = useNavigate();
-
-  const handleBackClick = () => {
-    navigate('/');
-  };
-
   return (
-    <div className="back-bar">
-      <button className="back-button" onClick={handleBackClick}>
-        <IoIosArrowBack />
-      </button>
+    <div className="backbar">
+      <div className="logo">
+        <Link to="/">Logo</Link>
+      </div>
     </div>
   );
 };
