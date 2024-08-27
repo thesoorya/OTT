fa618d75ea55ff712c45a351a2f35934e30f9ac7
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-27 16:47:06 +0530

    routes changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Signup from './pages/Auth/Signup'
-import Login from './pages/Auth/Login'
-import Home from './pages/Home/Home'
-import { Toaster } from 'react-hot-toast'
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Auth/Signup';
+import Login from './pages/Auth/Login';
+import Home from './pages/Home/Home';
+import { Toaster } from 'react-hot-toast';
+import { useAuthStore } from './components/Store/AuthUser';
 
 const App = () => {
+  const { user } = useAuthStore();
+
   return (
     <div>
       <Routes>
-        <Route path='/register' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={!user ? <Signup /> : <Navigate to='/' />} />
+        <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
+
       <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
