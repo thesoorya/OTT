63290dab308298bccfb607ce96777b86c4576a74
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-30 16:01:54 +0530

    wishlist function completed and cart system onprocess

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { FaStar } from "react-icons/fa";
 import { IoIosHeartEmpty, IoIosHeart } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
 import Loader from '../../components/Loader/Loader';
 import Navbar from '../../components/Navbar/Navbar';
 import './Product.css';
-import { useWishlist } from '../../components/Store/WishListAuth';
 import { useAuthStore } from '../../components/Store/AuthUser';
 
 const Product = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
+  const navigate = useNavigate()
 
-  const { wishlist, toggleWishlist, addWishList, removeWishList } = useWishlist();
-  const { user, authCheck } = useAuthStore();
+  const { user, wishlist, cart, addWishList, addToCart, getWishlist, authCheck, getCart, removeWishList } = useAuthStore();
+
+  const isProductInWishlist = wishlist?.includes(id);
+  const isProductInCart = cart?.some(item => item.productId.toString() === id);
 
   useEffect(() => {
     axios.get(`/api/product/getproductbyid/${id}`)
@@ -31,15 +35,38 @@ const Product = () => {
 
   useEffect(() => {
     authCheck();
-  }, [authCheck]);
+    getWishlist();
+    getCart();
+  }, [authCheck, getWishlist, getCart]);
 
   const handleWishlistClick = () => {
-    if (wishlist) {
-      removeWishList(user._id, id);
-    } else {
-      addWishList(user._id, id);
+    if (user) {
+      if (isProductInWishlist) {
+        removeWishList(user._id, id);
+      } else {
+        addWishList(user._id, id);
+      }
     }
-    toggleWishlist();
+  };
+
+  const handleAddToCart = () => {
+    if (user) {
+      if (!isProductInCart) {
+        addToCart(user._id, id, quantity);
+      } else {
+        addToCart(user._id, id, 1);
+      }
+    }
+
+    navigate('/cart')
+  };
+
+  const increaseQuantity = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    setQuantity(prevQuantity => Math.max(prevQuantity - 1, 1));
   };
 
   if (isLoading) {
@@ -51,16 +78,16 @@ const Product = () => {
       <Navbar />
       <div className='productPageContainer'>
         <div className="productPageImageContainer">
-          <div className="productWishLogo">
-            {wishlist ? (
-              <IoIosHeart onClick={handleWishlistClick} />
-            ) : (
-              <IoIosHeartEmpty onClick={handleWishlistClick} />
-            )}
-          </div>
           <img src={product.image} className='productPageImage' alt={product.title} />
         </div>
         <div className="productPageDetailsContainer">
+          {user && <div className="productWishLogo">
+            {isProductInWishlist ? (
+              <IoIosHeart style={{ color: 'red' }} onClick={handleWishlistClick} />
+            ) : (
+              <IoIosHeartEmpty onClick={handleWishlistClick} />
+            )}
+          </div>}
           <div className='productPageDetails'>
             <h1>{product.title}</h1>
             <h4>{product.category}</h4>
@@ -69,15 +96,20 @@ const Product = () => {
               <span><FaStar /> {product.rating?.rate}</span>{" "}
               <span>({product.rating?.count} reviews)</span>
             </div>
-            <p><small>{product.description}</small></p>
-          </div>
-          <div className="productPageButtons">
-            {/* Add buttons for actions like adding to cart or purchasing */}
+            <p>{product.description}</p>
+            <div className="productQuantityContainer">
+              <button className='quantityButton' onClick={decreaseQuantity}>-</button>
+              <span className='quantityNumber'>{quantity}</span>
+              <button className='quantityButton' onClick={increaseQuantity}>+</button>
+            </div>
+            <button className='addToCartBtn' onClick={handleAddToCart}>
+              Add to cart <FaCartShopping />
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Product;
--- client/src/pages/Wishlist/Wishlist.jsx
+++ client/src/pages/Wishlist/Wishlist.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+// Wishlist.jsx
+import React, { useEffect, useState } from 'react';
+import { useAuthStore } from '../../components/Store/AuthUser';
+import DisplayProduct from '../../components/DisplayProduct/DisplayProduct';
+import Loader from '../../components/Loader/Loader';
+import Navbar from '../../components/Navbar/Navbar';
+import axios from 'axios';
+import './Wishlist.css';
+import { Link } from 'react-router-dom';
 
 const Wishlist = () => {
+  const { user, authCheck, wishlist, getWishlist } = useAuthStore();
+  const [wishProducts, setWishProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    authCheck();  // Check authentication first
+    getWishlist();  // Fetch wishlist
+
+    const fetchWishlistProducts = async () => {
+      try {
+        const productRequests = wishlist.map(id => axios.get(`/api/product/getproductbyid/${id}`));
+        const productResponses = await Promise.all(productRequests);
+        const products = productResponses.map(res => res.data.content);
+        setWishProducts(products);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching wishlist products:", error);
+        setIsLoading(false);
+      }
+    };
+
+    if (wishlist.length > 0) {
+      fetchWishlistProducts();
+    } else {
+      setIsLoading(false);
+    }
+  }, [authCheck, getWishlist, wishlist]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <div>Wishlist</div>
-  )
+    <div className='wishlist'>
+      <Navbar />
+      <h1>Wishlist</h1>
+      <div className="wishlistContainer">
+        {wishProducts.length > 0 ? (
+          wishProducts.map((product) => (
+            <DisplayProduct key={product.id} product={product} />
+          ))
+        ) : (
+          user ? <p>Your wishlist is empty.</p> : <p>Please <Link to='/login'>Login / Sign up</Link> to view your wishlist.</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Wishlist
+export default Wishlist;
